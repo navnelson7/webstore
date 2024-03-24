09943baf802682238302668a791bf1e41eaca904
Juan Nelson Navarro Deleon <navnelson7@gr.local>
2024-03-23 23:12:32 -0600

    routes products, products-detail and checkout ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'products',
+    loadComponent: ()=> import('./modules/products/products.component')
+  },
+  {
+    path:'products-details/:id',
+    loadComponent: ()=> import('./modules/products/details/details.component')
+  },{
+    path:'checkout/',
+    loadComponent: ()=> import('./modules/checkout/checkout.component')
+  },
+  {path:'', redirectTo:'products', pathMatch:'full'},
+  {path:'**', redirectTo:'products', pathMatch:'full'}
+];
